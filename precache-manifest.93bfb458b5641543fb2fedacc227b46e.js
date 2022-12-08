ad4f313d7be6a6020b0349bc7f57a599fe4d37f6
kalyantvspv <kalyantvspv@gmail.com>
2022-12-09 00:06:50 +0530

    Updates

---
+++ precache-manifest.93bfb458b5641543fb2fedacc227b46e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc771818df04e381fd65080b0d85b2bc",
+    "url": "/keeperapp/index.html"
+  },
+  {
+    "revision": "c16b58a6bc5acba5e11e",
+    "url": "/keeperapp/static/js/2.c13a8499.chunk.js"
+  },
+  {
+    "revision": "d0fb798a9756ba8107b5",
+    "url": "/keeperapp/static/js/main.77ea0b5f.chunk.js"
+  },
+  {
+    "revision": "08d13058b8622ced82fe",
+    "url": "/keeperapp/static/js/runtime-main.04c51063.js"
+  }
+]);
